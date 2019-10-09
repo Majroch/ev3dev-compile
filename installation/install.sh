c4d4c929e060ccddb009ffb92d234d85ea8ab869
Majroch <jakuboch4@gmail.com>
2019-10-09 20:50:08 +0200

    Fixed

--- installation/install.sh
+++ installation/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo apt-get install autoconf make g++-arm-linux-gnueabi -y;
-echo "All required packages are installed!";
-echo "To compile a program, use command:";
-echo "	make";
+echo "";
+echo "Finished! All required packages are installed!";
