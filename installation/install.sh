d3c55b4e26057b2284e114f6d44d3d4cf3f06c0d
Majroch <jakuboch4@gmail.com>
2019-11-29 22:11:20 +0100

    Update install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get install autoconf make g++-arm-linux-gnueabi -y;
+sudo apt-get update && sudo apt-get install autoconf make g++-arm-linux-gnueabi -y;
 echo "";
 echo "Finished! All required packages are installed!";
